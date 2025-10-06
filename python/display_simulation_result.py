b54b4a8a53ef8f0ae1d22ef7b19f14335b7df2ad
jiyuehe <jiyue_he@163.com>
2025-10-06 12:34:29 -0700

    version to save

--- python/display_simulation_result.py
+++ python/display_simulation_result.py
@@ -121,7 +121,7 @@ if save_flag == 1:
             print(f'saving movie {(n+1)/n_time*100:.1f}%')
 
         plot_handle._offsets3d = (xyz[:, n, 0], xyz[:, n, 1], xyz[:, n, 2]) # update voxels positions
-        
+
         plot_handle.set_facecolor(map_color[n]) # set color based on phase to each voxel
         ax.set_title(f'Time: {n}/{n_time} ms') # set title with current time step
 
@@ -135,87 +135,3 @@ if save_flag == 1:
     anim.save('../result/simulation movie.mp4', writer=writer)
 
     print("movie saved as mp4")
-
-
-
-
-
-
-
-
-# do_flag = 1
-# if do_flag == 1:
-#     print("display movie")
-
-#     # dictionary to store view angles for each frame
-#     view_angles = {}
-
-#     interval = 0.01
-#     plt.figure(figsize=(10, 8))
-#     ax = plt.axes(projection='3d')
-#     for n in range(n_time):
-#         print(n/n_time)
-
-#         ax.clear()
-
-#         ax.scatter(xyz[:, n, 0], xyz[:, n, 1], xyz[:, n, 2], 
-#                    c=voltage[:, n], s=2, marker='.', alpha=1, cmap='coolwarm', vmin=v_min, vmax=v_max)
-            
-#         # set title with current time step
-#         ax.set_title(f'Time: {t[n]}/{t[-1]}')
-        
-#         # reset axis properties
-#         ax.set_xlabel('X')
-#         ax.set_ylabel('Y')
-#         ax.set_zlabel('Z')
-#         fn_set_axes_equal.execute(ax)
-#         ax.set_xlim([x_min, x_max])
-#         ax.set_ylim([y_min, y_max])
-#         ax.set_zlim([z_min, z_max])
-
-#         # capture current view angles
-#         elev = ax.elev  # elevation angle
-#         azim = ax.azim  # azimuth angle
-#         view_angles[n] = {'elev': elev, 'azim': azim}
-
-#         plt.pause(interval)
-
-#     # save simulation movie as mp4
-#     do_flag = 1
-#     if do_flag == 1:
-#         print("saving movie as mp4")
-
-#         fig = plt.figure(figsize=(10, 8))
-#         ax = plt.axes(projection='3d')
-
-#         def animate(n):
-#             print(n/n_time)
-
-#             ax.clear()
-            
-#             ax.scatter(xyz[:, n, 0], xyz[:, n, 1], xyz[:, n, 2], 
-#                     c=voltage[:, n], s=2, marker='.', alpha=1, cmap='coolwarm', vmin=v_min, vmax=v_max)
-                
-#             # set title with current time step
-#             ax.set_title(f'Time: {t[n]}/{t[-1]}')
-            
-#             # reset axis properties
-#             ax.set_xlabel('X')
-#             ax.set_ylabel('Y')
-#             ax.set_zlabel('Z')
-#             fn_set_axes_equal.execute(ax)
-#             ax.set_xlim([x_min, x_max])
-#             ax.set_ylim([y_min, y_max])
-#             ax.set_zlim([z_min, z_max])
-
-#             # restore view angle to maintain user's rotation
-#             ax.view_init(elev=view_angles[n]['elev'], azim=view_angles[n]['azim'])
-
-#         anim = animation.FuncAnimation(fig, animate, frames=n_time, interval=10, blit=False, repeat=False)
-#         # the interval parameter specifies the delay between frames in milliseconds
-
-#         # save
-#         writer = FFMpegWriter(fps=10, bitrate=1800)
-#         anim.save('../result/simulation movie.mp4', writer=writer)
-
-#         print("movie saved as mp4")
